6e236050960c87260bd23cd856762f8d12c5b5e6
Dina170 <dinaelhadidy966@gmail.com>
2025-05-11 15:27:07 +0300

    Create an Editor model and a join model PostEditor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_115509) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_11_121828) do
+  create_table "editors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_editors", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "editor_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["editor_id"], name: "index_post_editors_on_editor_id"
+    t.index ["post_id"], name: "index_post_editors_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -27,5 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_115509) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "post_editors", "editors"
+  add_foreign_key "post_editors", "posts"
   add_foreign_key "posts", "users", column: "creator_id"
 end
