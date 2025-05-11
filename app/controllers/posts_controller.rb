ad9abe35f1e298c0b58f30a477cf73a7c1e49f51
Dina170 <dinaelhadidy966@gmail.com>
2025-05-11 08:06:59 +0300

    Delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,11 @@ class PostsController < ApplicationController
         end
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path, notice: "post deleted."
+    end
 
     private
 
