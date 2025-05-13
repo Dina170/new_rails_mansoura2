0e1fabf2e6f3e078f6c592c23d3ba952abdf8363
Dina170 <dinaelhadidy966@gmail.com>
2025-05-13 12:32:33 +0300

    Update post model and controller to use creator_id; modify views accordingly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :post_editors
+  has_many :post_editors, dependent: :destroy
   has_many :editors, through: :post_editors
 end
