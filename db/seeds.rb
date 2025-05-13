f96f3721e611709d925319bcdc30f868dffaa49d
Dina170 <dinaelhadidy966@gmail.com>
2025-05-13 12:32:43 +0300

    Add seed data for users, editors, and posts with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+u1 = User.create!(name: "dina", email: "dina@test.com")
+u2 = User.create!(name: "sara", email: "sara@test.com")
+
+e1 = Editor.create!(name: "editor1")
+e2 = Editor.create!(name: "editor2")
+
+p1 = Post.create!(title: "post 1", content: "content 1", creator: u1)
+p2 = Post.create!(title: "post 2", content: "content 2", creator: u2)
+
+PostEditor.create!(post: p1, editor: e1)
+PostEditor.create!(post: p1, editor: e2)
+PostEditor.create!(post: p2, editor: e2)
